27ac6ad2c78b64cefe24baf70cdb9ea174161f32
Clara Ameller <claraameller7@gmail.com>
2018-03-06 23:28:28 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
 end
